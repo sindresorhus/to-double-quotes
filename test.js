2406826ff37b73454a1e658825975b3f3921d192
Shahar Talmi <shahar.talmi@gmail.com>
2014-04-12 02:49:27 +0300

    fix(regexp): eat also escaped charcters inside string

--- test.js
+++ test.js
@@ -13,4 +13,5 @@ it('should convert single-quotes to double-quotes', function () {
 	assert.equal(d('\'bar\' "foo" \'baz\''), '"bar" "foo" "baz"');
 	assert.equal(d('\\\'foo\\\''), '"foo"');
 	assert.equal(d('{\'a\':\'<a href=\\\'addr\\\'>\'}'), JSON.stringify({'a': '<a href=\'addr\'>'}));
+	assert.equal(d('{\'a\':\'aa\\n<a href=\\\'addr\\\'>\'}'), JSON.stringify({'a': 'aa\n<a href=\'addr\'>'}));
 });
