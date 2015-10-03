51003b68b1aa064c8bdbdac5f12c722ead2b4c2f
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-04 01:23:59 +0700

    drop `meow` temporarily
    for eslint/eslint#4031

--- cli.js
+++ cli.js
@@ -1,32 +1,20 @@
 #!/usr/bin/env node
 'use strict';
 var getStdin = require('get-stdin');
-var meow = require('meow');
 var toDoubleQuotes = require('./');
-
-var cli = meow({
-	help: [
-		'Usage',
-		'  $ to-double-quotes <string>',
-		'  $ echo <string> | to-double-quotes',
-		'',
-		'Example',
-		'  $ to-double-quotes "I love \'unicorns\'"',
-		'  I love "unicorns"'
-	]
-});
+var input = process.argv[2];
 
 function init(data) {
 	console.log(toDoubleQuotes(data));
 }
 
 if (process.stdin.isTTY) {
-	if (!cli.input[0]) {
+	if (!input) {
 		console.error('String required');
 		process.exit(1);
 	}
 
-	init(cli.input[0]);
+	init(input);
 } else {
 	getStdin(init);
 }
